4783cbd8979135d186bffe0379ebd24d5ff0436a
Timur Erdag <terdag@gmail.com>
2019-03-06 01:30:01 +0100

    initial commit with all cookiecutter project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use Deep Leaning to find Wally!',
+    author='Team Wally',
+    license='MIT',
+)
